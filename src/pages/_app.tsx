dc8409638003cb48f198dba12199775d54ceb5a6
Mauricio Porfirio <mauriciosporfirio@gmail.com>
2021-05-06 16:20:26 -0300

    finished project - Podcastr NLW5

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import { PlayerContextProvider } from '../contexts/PlayerContext'
+
+import styles from '../styles/app.module.scss'
+
+import '../styles/global.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  )
+}
+
+export default MyApp
